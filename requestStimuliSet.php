4dc01b1b4894382c0cd3306efbcf5b5c5e78bca5
Stephen Searles <stephen.searles@ucla.edu>
2011-01-22 22:08:17 -0800

    rewrote requestStimuliSet.php so that it would deliver empty groups

--- requestStimuliSet.php
+++ requestStimuliSet.php
@@ -1,52 +1,41 @@
 <?php
   include 'connect.php';
   $set = $_POST['set'];
-  $query = "SELECT stimulus_id,category1,category2,subcategory1,subcategory2,word,correct_response,instruction,mask,stimuli.`order` AS stimulusOrder,stimuliGroups.id AS group_id,stimuliGroups.name AS groupName,stimuliGroups.order AS groupOrder,stimuliGroups.randomize AS randomize FROM stimuli JOIN stimuliGroups ON stimuli.`group`=stimuliGroups.`id` WHERE `set`=$set ORDER BY groupOrder,stimulusOrder";
+  $query = "SELECT * FROM stimuliGroups WHERE `stimuliSet`=$set ORDER BY `order` ASC";
   $result = mysql_query($query);
-  $rows = mysql_num_rows($result);
-  $row = 0;
-  $group = mysql_result($result,$row, "group_id");
-  do {
-    while ($group != mysql_result($result, $row, "group_id")) {
-      if ($changed == true) {
-        $groupArray[] = array(
-          "group_id" => $group,
-          "randomize" => mysql_result($result,$row-1,"randomize"),
-          "groupName" => mysql_result($result,$row-1,"groupName"),
-          "stimuli" => $array
-        );
-        $changed = false;
-        unset($array);
-      }
-      $group++;
+  for ($row = 0; $row < mysql_num_rows($result); $row++) {
+    $group = mysql_result($result,$row,"id");
+    $query2 = "SELECT * FROM stimuli WHERE (`set`=$set AND `group`=$group)";
+    $result2 = mysql_query($query2);
+    $rows2 = mysql_num_rows($result2);
+    for ($row2 = 0; $row2 < mysql_num_rows($result2); $row2++) {
+      $array[] = array(
+        "stim_id" => mysql_result($result2, $row2, "stimulus_id"),
+        "category1" => mysql_result($result2, $row2, "category1"),
+        "category2" => mysql_result($result2, $row2, "category2"),
+        "subcategory1" => mysql_result($result2, $row2, "subcategory1"),
+        "subcategory2" => mysql_result($result2, $row2, "subcategory2"),
+        "word" => mysql_result($result2, $row2, "word"),
+        "correct_response" => mysql_result($result2, $row2, "correct_response"),
+        "instruction" => mysql_result($result2, $row2, "instruction"),
+        "mask" => mysql_result($result2, $row2, "mask")
+      );
     }
-    $changed = true;
-    $array[] = array(
-      "stim_id" => mysql_result($result, $row, "stimulus_id"),
-      "category1" => mysql_result($result, $row, "category1"),
-      "category2" => mysql_result($result, $row, "category2"),
-      "subcategory1" => mysql_result($result, $row, "subcategory1"),
-      "subcategory2" => mysql_result($result, $row, "subcategory2"),
-      "word" => mysql_result($result, $row, "word"),
-      "correct_response" => mysql_result($result, $row, "correct_response"),
-      "instruction" => mysql_result($result, $row, "instruction"),
-      "mask" => mysql_result($result, $row, "mask")
+    mysql_free_result($result2);
+    $groupArray[] = array(
+      "group_id" => $group,
+      "randomize" => mysql_result($result,$row,"randomize"),
+      "groupName" => mysql_result($result,$row,"name"),
+      "stimuli" => $array ? $array : array()
     );
-    $row++;
-  } while ($row < $rows);
-  
-  $groupArray[] = array(
-    "group_id" => $group,
-    "randomize" => mysql_result($result,$row-1,"randomize"),
-    "groupName" => mysql_result($result,$row-1,"groupName"),
-    "stimuli" => $array
-  );
-    
-  $upperArray["stimuliGroups"] = $groupArray;
+    unset($array);
+  }
   mysql_free_result($result);
+  $upperArray["stimuliGroups"] = $groupArray;
   $query = "SELECT COUNT(DISTINCT subj) AS responses FROM responses INNER JOIN stimuli ON responses.stimulus=stimuli.stimulus_id WHERE stimuli.`set`=$set";
   $result = mysql_query($query);
   $upperArray["responseCount"] = mysql_result($result, 0, "responses");
+  mysql_free_result($result);
   mysql_close();
   echo json_encode($upperArray);
 ?>
