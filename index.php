a6fda66b96a18a70e6a7ace0532425647f3cb81a
Stephen Searles <stephen.searles@ucla.edu>
2011-02-13 23:17:13 -0800

    changed index page for security

--- index.php
+++ index.php
@@ -10,12 +10,6 @@
   <body>
     <h1>IAT Web</h1>
     <h3>Instructions</h3>
-    <p>Once you begin, you there will be two categories at the top of your screen and a word in the center. You must select the category to which the word belongs. To select the category on the left, press e, and for the right, i. Keep your index fingers on these keys, because you must go categorize the words quickly.</p>
-    <table>
-      <tr><td><h3>Click the experiment to begin</h3></td></tr>
-      <tr><td><a href="IAT.php?s=1">Test 1</a></td></tr>
-      <tr><td><a href="IAT.php?s=2">Test 2</a></td></tr>
-    </table>
-    <p><a href="stimuliManager.php">Admin Tool</a>
+    <p>Welcome to the Shih Labs IAT website.</p>
   </body>
 </html>
