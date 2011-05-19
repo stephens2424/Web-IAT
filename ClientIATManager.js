77afec1e027d197e59d29239e12b5e3b9cf7603c
Stephen Searles <stephen.searles@gmail.com>
2011-05-19 16:34:08 -0700

    changed layout to css based model instead of table based

--- ClientIATManager.js
+++ ClientIATManager.js
@@ -145,85 +145,89 @@ var IAT = (function() {
         }));
       },
       generateStimuliTable : function() {
-        var $table = $('<table>').attr('id','stimuliGroupTable').addClass('stimuliGroupTable');
+        var $table = $('<div>').attr('id','stimuliGroupDiv').addClass('stimuliGroupDiv');
         for (var group in this.stimuliGroups) {
-          $table.append(this.groupRowFromObject(this.stimuliGroups[group]));
+          $table.append(this.groupFromObject(this.stimuliGroups[group]));
         }
         return $table;
       },
-      groupRowFromObject : function(group) {
-        var $row = $('<tr>');
-        var $cell = $('<td>');
-        var $innerTable = $('<table>').addClass('stimuliTable');
-        var $tableHeader = $('<thead>');
-        var $disclosureCell = $(DISCLOSURE_HEADER_CELL_STRING);
-        $disclosureCell.children('img').first().click(function() {
+      groupFromObject : function(group) {
+        var $groupDiv = $('<div>').addClass('stimuliGroup').corner();
+        var $groupHeader = $('<div>').addClass('stimuliGroupHeader');
+        var $stimuli = $('<div>').addClass('stimuliDiv');
+        var $disclosureSpan = $(DISCLOSURE_HEADER_STRING);
+        $disclosureSpan.children('img').first().click(function() {
           if (group.disclosed === undefined | group.disclosed === null) {
             group.disclosed = false;
           }
           group.disclosed = !group.disclosed;
-          $row.find('tbody').first().fadeToggle().find('div').slideToggle();
-          if (group.disclosed) $row.find('img').first().animate({rotate : '-90deg'});
-          else $row.find('img').first().animate({rotate : '0deg'});
+          $stimuli.slideToggle();
+          if (group.disclosed) $groupDiv.find('img').first().animate({rotate : '-90deg'});
+          else $groupDiv.find('img').first().animate({rotate : '0deg'});
         });
-        $tableHeader.append($disclosureCell);
-        var $stimuliHeader = $('<th>').addClass('stimuliHeader');
+        $groupHeader.append($disclosureSpan);
+        var $stimuliHeader = $('<span>').addClass('stimuliHeader');
         $stimuliHeader.text(group.name);
-        $tableHeader.append($stimuliHeader);
-        $tableHeader.append($('<th>').addClass('greenwaldHeader').text('greenwald selector'));
-        $tableHeader.append($('<th>').addClass('groupActionHeader').text('action selector'));
-        $tableHeader.append($('<th>').addClass('randomization').text('randomization box'));
-        $innerTable.append($tableHeader);
+        $groupHeader.append($stimuliHeader);
+        $groupHeader.append($('<span>').addClass('greenwaldHeader').text('greenwald selector'));
+        $groupHeader.append($('<span>').addClass('groupActionHeader').text('action selector'));
+        $groupHeader.append($('<span>').addClass('randomization').text('randomization box'));
+        $groupDiv.append($groupHeader);
         for (var stimulus in group.stimuli) {
-          $innerTable.append(this.stimulusRowFromObject(group.stimuli[stimulus]));
+          $stimuli.append(this.stimulusDivFromObject(group.stimuli[stimulus]));
         }
-        $cell.append($innerTable);
-        $row.append($cell);
-        return $row;
+        $groupDiv.append($stimuli);
+        return $groupDiv;
       },
-      stimulusRowFromObject : function(stimulus) {
-        var $row = $('<tr>').addClass('stimulusRow');
-        var $emptyShifterCell = $('<td>');
-        var $stimulusEditButtonCell = $('<td>').append($('<div>').append('TODO - edit button')).addClass('midRowRounding');
-        var $stimulusDataCell = $('<td>').append($('<div>').append(this.stimulusDataTableFromObject(stimulus))).addClass('beginRowRounding');
-        var $stimulusActionsCell = $('<td>').append($('<div>').append('TODO - stimulus actions')).addClass('endRowRounding');
-        $row.append($emptyShifterCell).append($stimulusDataCell).append($stimulusEditButtonCell).append($stimulusActionsCell);
-        return $row;
+      stimulusDivFromObject : function(stimulus) {
+        var $stimulus = $('<div>').addClass('stimulus');
+        var $stimulusData = this.stimulusDataFromObject(stimulus).addClass('floatLeft');
+        var $stimulusOptions = $('<span>').addClass('floatRight');
+        var $stimulusEditButton = $('<div>').append('TODO - edit button');
+        var $stimulusActions = $('<div>').append('TODO - stimulus actions');
+        var $clear = $('<div>').addClass('clear');
+        $stimulusOptions.append($stimulusEditButton).append($stimulusActions);
+        $stimulus.append($stimulusOptions).append($stimulusData).append($clear);
+        return $stimulus;
       },
-      stimulusDataTableFromObject : function(stimulus) {
-        var $table = $('<table>').addClass('stimulusDataTable');
-        var $topRow = $('<tr>');
-        var $middleRow = $('<tr>');
-        var $bottomRow = $('<tr>');
+      stimulusDataFromObject : function(stimulus) {
+        var $table = $('<span>').addClass('stimulusData');
+        var $categoryDiv = $('<div>');
+        var $leftSpan = $('<span>').addClass('floatLeft');
+        var $rightSpan = $('<span>').addClass('floatRight');
+        var $centerDiv = $('<span>').addClass('center');
+        var $clear = $('<div>').addClass('clear');
         
-        var $cat1cell = $('<td>').addClass('stimulusDataCell').addClass('leftCategoryCell').addClass('topCategoryCell');
-        $cat1cell.text(this.categoryNameFromId(stimulus.category1));
-        var $cat2cell = $('<td>').addClass('stimulusDataCell').addClass('rightCategoryCell').addClass('topCategoryCell');
-        $cat2cell.text(this.categoryNameFromId(stimulus.category2));
-        var $subcat1cell = $('<td>').addClass('stimulusDataCell').addClass('leftCategoryCell').addClass('bottomCategoryCell');
-        $subcat1cell.text(this.categoryNameFromId(stimulus.subcategory1));
-        var $subcat2cell = $('<td>').addClass('stimulusDataCell').addClass('rightCategoryCell').addClass('bottomCategoryCell');
-        $subcat2cell.text(this.categoryNameFromId(stimulus.subcategory2));
-        var $wordCell = $('<td colspan="2">').addClass('stimulusDataCell').addClass('wordCell');
-        $wordCell.text(stimulus.word);
+        var $cat1 = $('<div>').addClass('stimulusDatum').addClass('leftCategory').addClass('topCategory');
+        $cat1.text(this.categoryNameFromId(stimulus.category1));
+        var $cat2 = $('<div>').addClass('stimulusDatum').addClass('rightCategory').addClass('topCategory');
+        $cat2.text(this.categoryNameFromId(stimulus.category2));
+        var $subcat1 = $('<div>').addClass('stimulusDatum').addClass('leftCategory').addClass('bottomCategory');
+        $subcat1.text(this.categoryNameFromId(stimulus.subcategory1));
+        var $subcat2 = $('<div>').addClass('stimulusDatum').addClass('rightCategory').addClass('bottomCategory');
+        $subcat2.text(this.categoryNameFromId(stimulus.subcategory2));
+        var $word = $('<div>').addClass('stimulusDatum').addClass('stimulusWord');
+        $word.text(stimulus.word);
         
-        $topRow.append($cat1cell).append($cat2cell);
-        $middleRow.append($subcat1cell).append($subcat2cell);
-        $bottomRow.append($wordCell);
-        $table.append($topRow).append($middleRow).append($bottomRow);
+        $leftSpan.append($cat1).append($subcat1);
+        $rightSpan.append($cat2).append($subcat2);
+        $categoryDiv.append($leftSpan).append($rightSpan).append($clear);
+        $centerDiv.append($word);
+        $table.append($categoryDiv).append($centerDiv);
         return $table;
       },
+      //dynamic actions
       //translations
       groupIdFromIndex : function(index) {
         return this.stimuliGroups[index].id;
       },
       categoryNameFromId : function(id) {
-        if (id === "0" | id === null | id === undefined) return "—";
+        if (id === "0" | id === null | id === undefined) return "\u2013";
         else return this.stimulusCategories[id];
       }
   };
   
-  const DISCLOSURE_HEADER_CELL_STRING = '<th class="disclosure"><img src="disclosureTriangle.png"></th>';
+  const DISCLOSURE_HEADER_STRING = '<span class="disclosure"><img src="disclosureTriangle.png"></span>';
 
   function requestExperiment(experimentNumber,callback) {
     var experimentPromise = $.Deferred().done(callback);
