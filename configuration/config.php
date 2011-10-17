b473d3547a61432e23adfdcde31378f81a98b89e
Stephen Searles <stephen.searles@gmail.com>
2011-10-16 18:48:02 -0700

    fixed gitignore to properly ignore config.php

--- configuration/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$databaseDomain = '127.0.0.1';
-$databaseUsername = 'root';
-$databasePassword = 'tempest24';
-$databaseName = 'testIAT';
-
-$baseURL = 'http://127.0.0.1/~Stephen/IATWeb';
-?>
